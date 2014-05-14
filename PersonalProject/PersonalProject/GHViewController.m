9b105b195a13d53f33b6e31b2a3b306e018e8616
guhun <tianranwuwai@yeah.net>
2014-05-14 10:13:45 +0800

    add a new project

---
+++ PersonalProject/PersonalProject/GHViewController.m
@@ -0,0 +1,29 @@
+//
+//  GHViewController.m
+//  PersonalProject
+//
+//  Created by Dana on 14-5-14.
+//  Copyright (c) 2014年 A. All rights reserved.
+//
+
+#import "GHViewController.h"
+
+@interface GHViewController ()
+
+@end
+
+@implementation GHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
